817b1598471a88529dc53431c1aa43f04d3275c8
algajula <allabakash.gajula@gmail.com>
2025-10-08 02:11:58 +0530

    react js initial setup 1105

--- src/App.js
+++ src/App.js
@@ -10,17 +10,27 @@ const apiUrl = process.env.REACT_APP_API_URL;
 console.log(`API URL: ${apiUrl}`);
 const envname = process.env.REACT_APP_ENV_NAME;
 console.log(`ENV NAME: ${envname}`);
-
 const reactjslogo = require('./images/reactjs.png');
+
+const Home = () => <h2>Home Page</h2>;
+const Customer = () => <h2>Customer Page</h2>;
+const Book = () => <h2>Book Page</h2>;
+const Searchcustomer = () => <h2>Search Customer Info</h2>;
+const Addcustomer = () => <h2>Add/Edit Customer Info</h2>;
+const Searchbook = () => <h2>Search Book Details</h2>;
+const Editbook = () => <h2>Add/Edit Book Info</h2>;
+const SearchS3file = () => <h2>Search AWS S3 file</h2>;
+const Uploads3file = () => <h2>Upload AWS S3 file</h2>;
+const Contact = () => <h2>Contact Us</h2>;
 
 function App() {
 
   const tabData = [
-      { id: 'tab1', label: 'Home', content: 'Main Content.' },
-      { id: 'tab2', label: 'Customer', content: 'Customer info' },
-      { id: 'tab3', label: 'Book', content: 'Books Info' },
-      { id: 'tab4', label: 'Aws S3', content: 'AWS S3 files info' },
-      { id: 'tab5', label: 'Contact', content: 'Api Info' }
+      { id: 'HOME', label: 'Home', content: 'Main Content.' },
+      { id: 'Customer', label: 'Customer', content: 'Customer info' },
+      { id: 'Book', label: 'Book', content: 'Books Info' },
+      { id: 'AWSS3', label: 'Aws S3', content: 'AWS S3 files info' },
+      { id: 'Contact', label: 'Contact', content: 'Api Info' }
     ];
 
   return (
@@ -30,37 +40,23 @@ function App() {
                   React {envname} environment
             <img src={reactjslogo} height="80px" width="270"/></h1>
         </header>
-        <div className="customtabs">
+        <Router>
           <Tabs tabs={tabData} />
-       </div>
-
-        <div class="navbar">
-            <a href="/home">Home</a>
-            <div class="subnav">
-                <button class="subnavbtn">Customer <i class="fa fa-caret-down"></i></button>
-                <div class="subnav-content">
-                    <a href="/customer/searchcustomer">Search Customer</a>
-                    <a href="/customer/editcustomer">Add Customer</a>
-                </div>
-            </div>
-            <div class="subnav">
-                <button class="subnavbtn">Aws S3 <i class="fa fa-caret-down"></i></button>
-                <div class="subnav-content">
-                    <a href="/awss3/searcs3files/">Search File</a>
-                    <a href="/awss3/uploads3file">Upload File</a>
-                </div>
-            </div>
-            <div class="subnav">
-                <button class="subnavbtn">Book <i class="fa fa-caret-down"></i></button>
-                <div class="subnav-content">
-                    <a href="/book/searchbook">Search Book</a>
-                    <a href="/book/editbook">Add Book</a>
-                </div>
-            </div>
-            <a href="/contactus">Contact</a>
-        </div>
-
-
+          <div className="tab-content">
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/customer" element={<Customer />} />
+              <Route path="/book" element={<Book />} />
+              <Route path="/customer/searchcustomer" element={<Searchcustomer />} />
+              <Route path="/customer/editcustomer" element={<Addcustomer />} />
+              <Route path="/book/searchbook" element={<Searchbook />} />
+              <Route path="/book/editbook" element={<Editbook />} />
+              <Route path="/awss3/searcs3files" element={<SearchS3file />} />
+              <Route path="/awss3/uploads3file" element={<Uploads3file />} />
+              <Route path="/contactus" element={<Contact />} />
+            </Routes>
+          </div>
+        </Router>
 
     </div>
   );
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,39 +1,61 @@
 import classes from './tabs.css';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export interface TabsProps {}
 
+function Tabs(){
 
-const Tabs = ({ tabs }) => {
-  const [activeTab, setActiveTab] = useState(tabs[0].id); // Set initial active tab
-
-  const handleTabClick = (tabId) => {
-    setActiveTab(tabId);
+  const [activeTab, setActiveTab] = useState('home'); // Set initial active tab
+  const navigate = useNavigate();
+  const goToRoutePage = (tabName, path) => {
+        console.log('path--',path)
+        console.log('tabName---',tabName)
+        setActiveTab(tabName);
+        navigate(path);
   };
 
   return (
       <div className="tabs-container">
         <div className="tab-buttons">
-          {tabs.map((tab) => (
-            <button
-              key={tab.id}
-              className={`tab-button ${activeTab === tab.id ? 'active' : ''}`}
-              onClick={() => handleTabClick(tab.id)} >
-              {tab.label}
-            </button>
-          ))}
-        </div>
-        <div className="tab-content">
-          {tabs.map((tab) => (
-            <div
-              key={tab.id}
-              className={`tab-pane ${activeTab === tab.id ? 'active' : ''}`}>
-              {tab.content}
-            </div>
-          ))}
-        </div>
+        <button
+          className={activeTab === 'home' ? 'active' : ''}
+          onClick={() => goToRoutePage('home', '/home')} > Home</button>
+        <button
+          className={activeTab === 'customer' ? 'active' : ''}
+          onClick={() => goToRoutePage('book', '/customer')} > Customer</button>
+        <button
+          className={activeTab === 'book' ? 'active' : ''}
+          onClick={() => goToRoutePage('book', '/book')} > Book</button>
+
+      <div class="navbar">
+          <a href="/home">Home</a>
+          <div class="subnav">
+              <button class="subnavbtn">Customer <i class="fa fa-caret-down"></i></button>
+              <div class="subnav-content">
+                  <a href="/customer/searchcustomer">Search Customer</a>
+                  <a href="/customer/editcustomer">Add Customer</a>
+              </div>
+          </div>
+          <div class="subnav">
+              <button class="subnavbtn">Aws S3 <i class="fa fa-caret-down"></i></button>
+              <div class="subnav-content">
+                  <a href="/awss3/searcs3files/">Search File</a>
+                  <a href="/awss3/uploads3file">Upload File</a>
+              </div>
+          </div>
+          <div class="subnav">
+              <button class="subnavbtn">Book <i class="fa fa-caret-down"></i></button>
+              <div class="subnav-content">
+                  <a href="/book/searchbook">Search Book</a>
+                  <a href="/book/editbook">Add Book</a>
+              </div>
+          </div>
+          <a href="/contactus">Contact</a>
+      </div>
+
+      </div>
       </div>
     );
-  };
-
+}
 export default Tabs;
