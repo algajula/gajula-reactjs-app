8d896d6e7b7c199978453741c758d0173edc74e1
algajula <allabakash.gajula@gmail.com>
2025-10-07 23:38:04 +0530

    react js initial setup 1103

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import './App.css';
 import Tabs from './components/Tabs';
 import './components/tabs.css';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+console.log(`API URL: ${apiUrl}`);
+const envname = process.env.REACT_APP_ENV_NAME;
+console.log(`ENV NAME: ${envname}`);
+
+const reactjslogo = require('./images/reactjs.png');
+
 function App() {
 
   const tabData = [
-      { id: 'tab1', label: 'About Us', content: 'This is the content for About Us.' },
-      { id: 'tab2', label: 'Services', content: 'Explore our wide range of services.' },
-      { id: 'tab3', label: 'Contact', content: 'Get in touch with us.' },
+      { id: 'tab1', label: 'Home', content: 'Main Content.' },
+      { id: 'tab2', label: 'Customer', content: 'Customer info' },
+      { id: 'tab3', label: 'Book', content: 'Books Info' },
+      { id: 'tab4', label: 'Aws S3', content: 'AWS S3 files info' },
+      { id: 'tab5', label: 'Contact', content: 'Api Info' }
     ];
 
   return (
     <div className="App">
+       <header className="App-header">
+           <h1><img src={reactjslogo} height="80px" width="270"/>
+                  React {envname} environment
+            <img src={reactjslogo} height="80px" width="270"/></h1>
+        </header>
        <div className="customtabs">
-          <h1>Dynamic React Tabs</h1>
           <Tabs tabs={tabData} />
        </div>
     </div>
